88f6760fe7c471f9ab5d97280f3829340473af23
Gerardo Cruz <gerardo2551.cr@gmail.com>
2021-09-07 20:29:52 -0500

    SegundoCommit

---
+++ AccesoDatos.tienda/TiendaAccesoDatos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Tienda;
+
+namespace AccesoDatos.tienda
+{
+    public class TiendaAccesoDatos
+    {
+        ConexionAccesoDatos _conexion;
+        public TiendaAccesoDatos()
+        {
+            try
+            {
+                _conexion = new ConexionAccesoDatos("localhost", "root", "1234", "tienda", 3306);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Falló la conexion" + ex.Message);
+            }
+        }
+        public void GuardarProducto(Productos producto)
+        {
+            try
+            {
+                string consulta = string.Format("insert into producto values({0},'{1}','{2}','{3}')", producto.Idproducto, producto.Nombre, producto.Descripcion, producto.Precio);
+                _conexion.EjecutarConsulta(consulta);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Falló el guardado" + ex.Message);
+            }
+        }
+    }
+}
---
+++ Tienda/Productos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda
+{
+    public class Productos
+    {
+        private string _idproducto;
+        private string _nombre;
+        private string _descripcion;
+        private string _precio;
+
+        public string Idproducto { get => _idproducto; set => _idproducto = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public string Precio { get => _precio; set => _precio = value; }
+    }
+}
